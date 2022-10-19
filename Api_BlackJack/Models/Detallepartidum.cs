d0aba333f2103539b905ad3f7b32d97b8bf3ff52
InakiEguia113128 <90732757+InakiEguia113128@users.noreply.github.com>
2022-10-19 10:36:51 -0300

    Scafolding

---
+++ Api_BlackJack/Models/Detallepartidum.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace Api_BlackJack.Models
+{
+    public partial class Detallepartidum
+    {
+        public int Id { get; set; }
+        public int IdPartida { get; set; }
+        public int IdCarta { get; set; }
+
+        public virtual Carta IdCartaNavigation { get; set; } = null!;
+        public virtual Juego IdPartidaNavigation { get; set; } = null!;
+    }
+}
---
+++ Api_BlackJack/Models/Juego.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace Api_BlackJack.Models
+{
+    public partial class Juego
+    {
+        public Juego()
+        {
+            Detallepartida = new HashSet<Detallepartidum>();
+        }
+
+        public int Id { get; set; }
+        public int IdUsuario { get; set; }
+        public DateTime Fecha { get; set; }
+        public ulong Activo { get; set; }
+
+        public virtual Usuario IdUsuarioNavigation { get; set; } = null!;
+        public virtual ICollection<Detallepartidum> Detallepartida { get; set; }
+    }
+}

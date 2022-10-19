e7731a45c69952f229dd92ad3169d0f7ad95d9d9
InakiEguia113128 <90732757+InakiEguia113128@users.noreply.github.com>
2022-10-19 10:56:39 -0300

    Swagger

---
+++ Api_BlackJack/Controllers/UsuarioController.cs
@@ -0,0 +1,22 @@
+﻿using Api_BlackJack.DataContext;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Api_BlackJack.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class UsuarioController : ControllerBase
+    {
+        private readonly BlackJackContext context;
+        public UsuarioController(BlackJackContext _context)
+        {
+            this.context = _context;
+        }
+
+    }
+}
--- Api_BlackJack/Program.cs
+++ Api_BlackJack/Program.cs
@@ -1,14 +1,37 @@
+using Api_BlackJack.DataContext;
+using Microsoft.AspNetCore.Builder;
 using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.Hosting;
+using System;
+
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
 builder.Services.AddRazorPages();
-
+builder.Services.AddControllers();
+builder.Services.AddEndpointsApiExplorer();
+builder.Services.AddSwaggerGen();
+builder.Services.AddCors();
+builder.Services.AddEntityFrameworkMySql().AddDbContext<BlackJackContext>(options => options.UseMySQL("Server=localhost; Database=BlackJack; Port=3306;User Id=root; Password=45262;"));
+AppContext.SetSwitch("MySql.EnableLegacyTimestampBehavior", true);
 var app = builder.Build();
 
 
-
 // Configure the HTTP request pipeline.
+if (app.Environment.IsDevelopment())
+{
+    app.UseSwagger();
+    app.UseSwaggerUI();
+}
+
+app.UseCors(c =>
+{
+    c.AllowAnyHeader();
+    c.AllowAnyMethod();
+    c.AllowAnyOrigin();
+});
+
 if (!app.Environment.IsDevelopment())
 {
     app.UseExceptionHandler("/Error");
